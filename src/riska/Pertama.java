a95f0077060b84342d02b0b280996c3039e8e2e6
Riska439 <57169485+Riska439@users.noreply.github.com>
2020-06-04 19:15:32 +0700

    RISKA

---
+++ src/riska/Pertama.java
@@ -0,0 +1,16 @@
+package riska;
+public class Pertama {
+    public static void main(String[] args) {
+    try {
+        System.out.print("Input Kata : ");
+        char n = (char) System.in.read();
+        System.out.println("Hasil : " +n);
+
+        //Input  : Saya
+        //Output : S
+
+    } catch (Exception e) {
+        //TODO: handle exception
+    }
+}
+}
